407d2251ef1111f909c3b0f9595b841186937c86
ladydanicorn <dbronsonart@gmail.com>
2025-05-21 06:20:27 -0400

    Added App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CommentForm from './CommentForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: '600px', margin: '2rem auto', padding: '1rem' }}>
+      <h1>Comment App</h1>
+      <CommentForm />
     </div>
   );
 }
